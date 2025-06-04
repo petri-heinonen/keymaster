f13513bd45070dcbcb0004138fdd0b1121462736
Petri Heinonen <petri.heinonen@op.fi>
2025-06-04 09:10:01 +0300

    Fix warnings

--- src/main/java/fi/pmh/keymaster/persistence/KeyRepository.java
+++ src/main/java/fi/pmh/keymaster/persistence/KeyRepository.java
@@ -6,11 +6,9 @@ import org.springframework.data.jpa.repository.Query;
 import java.util.List;
 
 public interface KeyRepository extends JpaRepository<Key, Long> {
+    Key findByKeyId(String keyId);
     List<Key> findByClientIdOrderByCreatedAtDesc(String clientId);
     List<Key> findByClientIdAndIsPublishedTrueOrderByCreatedAtDesc(String clientId);
-    Key findByKeyId(String keyId);
-
-    List<Key> findAllByOrderByClientIdAscCreatedAtDesc();
 
     @Query("SELECT DISTINCT k.clientId FROM Key k ORDER BY k.clientId ASC")
     List<String> findAllClients();
